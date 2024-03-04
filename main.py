f287ee86b838c442a5ba652cd8f8f580d5a03b30
Code Keper <aliswork1122@gmail.com>
2024-03-04 23:12:20 +0500

    Placing the final result in single frame

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cv2
+import numpy as np
 
 # read the image
 actual_image = cv2.imread('flower_image.png')
 
-# specifing image new size
+# specifying image new size
 desire_size = (500, 500)
 
 # called resize function
@@ -18,11 +19,6 @@ font_color = (66, 235, 88)
 thickness = 1
 cv2.putText(actual_image, text, position, font, font_scale, font_color, thickness, cv2.LINE_AA)
 
-# showing the actual_image
-cv2.imshow('Actual Images', actual_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
 # add text upon resized_image
 text = "Output Image"
 position = (150, 50)
@@ -32,7 +28,20 @@ font_color = (66, 235, 88)
 thickness = 1
 cv2.putText(resized_image, text, position, font, font_scale, font_color, thickness, cv2.LINE_AA)
 
-# showing the resized_image
-cv2.imshow('Resized Image', resized_image)
+# setting width & height for canvas(Background):
+total_width = actual_image.shape[1] + resized_image.shape[1]
+total_height = max(actual_image.shape[0], resized_image.shape[0])
+
+# create a black background:
+create_canvas = np.zeros((total_height, total_width, 3), dtype=np.uint8)
+
+# placing image on the left side of the canvas:
+create_canvas[:actual_image.shape[0], :actual_image.shape[1]] = actual_image
+
+# placing image on the right side of the canvas:
+create_canvas[:resized_image.shape[0], actual_image.shape[1]:] = resized_image
+
+# showing the final results:
+cv2.imshow("concatenated_image", create_canvas)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
